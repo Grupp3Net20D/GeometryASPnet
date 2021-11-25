a334d3f8dc26ba906373d12223b383123bcb6d00
Binett <73723725+Binett@users.noreply.github.com>
2021-11-25 15:46:00 +0100

    Added class Romb

---
+++ GeometriTDD/Geometry/Romb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometriTDD.Geometry
+{
+    /// <summary>
+    /// Klass som räknar ut arean och omkretsen av en romb, ärver ifrån GeometricThing
+    /// </summary>
+    public class Romb : GeometricThing
+    {
+        /// <summary>
+        /// Properties för höjden och längden
+        /// </summary>
+        public float Heigth { get; set; }
+        public float Length { get; set; }
+      
+        public Romb()
+        {
+
+        }
+
+        public Romb(float heigth, float length)
+        {
+            Heigth = heigth;
+            Length = length;
+        }
+
+
+
+        public override float GetArea()
+        {
+            return Heigth * Length;
+        }
+
+        public override float GetPerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
