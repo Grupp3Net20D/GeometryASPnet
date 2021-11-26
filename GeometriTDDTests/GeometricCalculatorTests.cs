8092113d31a8545461b4c26eebec880af9e16151
Binett <73723725+Binett@users.noreply.github.com>
2021-11-26 09:09:09 +0100

    Added rhomus test

--- GeometriTDDTests/GeometricCalculatorTests.cs
+++ GeometriTDDTests/GeometricCalculatorTests.cs
@@ -199,5 +199,57 @@ namespace GeometriTDD.Tests
             Assert.AreEqual(expected, actual);
         }
 
+
+        [TestMethod]
+        [DataRow(1f, 1f, 1f)]
+        [DataRow(2f, 2f, 4f)]
+        public void Rhombus_CalculatesArea_ReturnsArea(float heigth, float side, float expected)
+        {
+            //Arrange 
+            var rhombus = new Romb(heigth, side);
+            //Act
+            var actual = rhombus.GetArea();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(1f, 1f, 4f)]
+        [DataRow(2f, 2f, 8f)]
+        public void Rhombus_CalculatesPerimeter_ReturnsArea(float heigth, float side, float expected)
+        {
+            //Arrange 
+            var rhombus = new Romb(heigth, side);
+            //Act
+            var actual = rhombus.GetPerimeter();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(1f, null, 0f)]
+        [DataRow(0f, 2f, 0f)]
+        public void Rhombus_CalculatesPerimeterWithNegativeValues_ReturnsZero(float heigth, float side, float expected)
+        {
+            //Arrange 
+            var rhombus = new Romb(heigth, side);
+            //Act
+            var actual = rhombus.GetPerimeter();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(1f, null, 0f)]
+        [DataRow(0f, 2f, 0f)]
+        public void Rhombus_CalculatesAreaWithNegativeValues_ReturnsZero(float heigth, float side, float expected)
+        {
+            //Arrange 
+            var rhombus = new Romb(heigth, side);
+            //Act
+            var actual = rhombus.GetPerimeter();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
