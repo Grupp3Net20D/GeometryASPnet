ad95df4e4bc4bd3996269a9837d1cae464f52339
myrlejon <60348075+myrlejon@users.noreply.github.com>
2021-11-26 16:28:08 +0100

    Added area calculation for Pentagon

--- GeometriTDDTests/GeometryTests/PentagonTests.cs
+++ GeometriTDDTests/GeometryTests/PentagonTests.cs
@@ -4,11 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GeometriTDD;
+using GeometriTDD.Geometry;
 
 namespace GeometriTDDTests.GeometryTests
 {
     [TestClass()]
     public class PentagonTests
     {
+        [TestMethod()]
+        public void GetAreaOnePentagon()
+        {
+            var calc = new GeometricCalculator();
+            float expected = 172.05f;
+            var pentagon = new Pentagon(10);
+            var actual = calc.GetArea(pentagon);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
