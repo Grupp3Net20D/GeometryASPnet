f1bc7411d0dca4081d0afdd80f06f15aad592896
Binett <73723725+Binett@users.noreply.github.com>
2021-11-29 11:55:14 +0100

    refactored circletests

--- GeometriTDDTests/GeometricCalculatorTests.cs
+++ GeometriTDDTests/GeometricCalculatorTests.cs
@@ -17,7 +17,7 @@ namespace GeometriTDD.Tests
         public void GetAreaTest()
         {
             var calc = new GeometricCalculator();
-            float expected = 428.53983f; // 100 + 78.53983 + 200 + 50 = 428.53983
+            float expected = 428.54f; // 100 + 78.53983 + 200 + 50 = 428.54
             var actual = calc.GetArea(new GeometricThing[]
             {
                 new Geometry.Square(10),
@@ -92,7 +92,7 @@ namespace GeometriTDD.Tests
         public void GetPerimeterTest()
         {
             var calc = new GeometricCalculator();
-            float expected = 161.41592f; // 40 + 31.41592 + 60 + 30 = 161.41592
+            float expected = 161.42f; // 40 + 31.41592 + 60 + 30 = 161.42
             var actual = calc.GetPerimeter(new GeometricThing[]
             {
                 new Geometry.Square(10),
--- GeometriTDDTests/GeometryTests/CircleTests.cs
+++ GeometriTDDTests/GeometryTests/CircleTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using GeometriTDD.Geometry;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,54 @@ namespace GeometriTDDTests.GeometryTests
     [TestClass()]
     public class CircleTests
     {
+        [TestMethod]
+        [DataRow(1f, 3.14f)]
+        public void Circle_CalculatesArea_ReturnsArea(float radie, float expected)
+        {
+            //Arrange 
+            var circle = new Circle(radie);
+            //Act
+            var actual = circle.GetArea();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(2f, 12.57f)]
+        public void Circle_CalculatesPerimeter_ReturnsArea(float radie, float expected)
+        {
+            //Arrange 
+            var circle = new Circle(radie);
+            //Act
+            var actual = circle.GetPerimeter();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(1f, null, 0f)]
+        [DataRow(0f, 2f, 0f)]
+        public void Circle_CalculatesPerimeterWithNegativeValues_ReturnsZero(float heigth, float side, float expected)
+        {
+            //Arrange 
+            var rhombus = new Romb(heigth, side);
+            //Act
+            var actual = rhombus.GetPerimeter();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(1f, null, 0f)]
+        [DataRow(0f, 2f, 0f)]
+        public void Circle_CalculatesAreaWithNegativeValues_ReturnsZero(float heigth, float side, float expected)
+        {
+            //Arrange 
+            var rhombus = new Romb(heigth, side);
+            //Act
+            var actual = rhombus.GetPerimeter();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
