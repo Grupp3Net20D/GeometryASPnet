c7be6000234501276613fdc4fd2a7dcef9b8661d
toveseger <tove.seger@outlook.com>
2021-11-29 11:12:17 +0100

    Design according to mock done

---
+++ GeometryGrupp3/Models/Circle.cs
@@ -0,0 +1,13 @@
+﻿namespace GeometryGrupp3.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class Circle
+    {
+        public int radius;
+        public int diameter;
+    }
+}
