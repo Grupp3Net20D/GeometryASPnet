6c8962e0d3709dc2626899531c502add4627ddfe
Mikael Larsson <73987306+84miklar@users.noreply.github.com>
2021-12-01 14:24:02 +0100

    Added contactform on a new branch

---
+++ GeometryGrupp3/Models/ContactForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeometryGrupp3.Models
+{
+    public class ContactForm
+    {
+        public string Fname { get; set; }
+
+        public string Lname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Msg { get; set; }
+
+        public bool HasSubscribedToNewsLetter { get; set; }
+    }
+}
