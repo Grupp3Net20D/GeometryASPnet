65c2a5f0be81a8740a8b23a8efb7af16d297ab05
Josefine <86josron@gafe.molndal.se>
2021-12-01 15:51:28 +0100

    triangelcontroller

---
+++ GeometryGrupp3/Controllers/TriangleController.cs
@@ -0,0 +1,32 @@
+﻿using GeometriTDD.Geometry;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeometryGrupp3.Controllers
+{
+    public class TriangleController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(Triangle triangle)
+        {
+            var result = triangle.GetArea();
+            ViewData["results"] = result;
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+        public IActionResult SaveDetail(float side)
+        {
+            return View();
+        }
+
+
+    }
+}
