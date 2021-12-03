10edf02f67086acc67c1998eb313bac6f5f4e4b1
Mikael Larsson <73987306+84miklar@users.noreply.github.com>
2021-12-03 13:59:20 +0100

    Implemented square view, no logic.

---
+++ GeometryGrupp3/Controllers/SquareController.cs
@@ -0,0 +1,30 @@
+﻿using GeometriTDD.Geometry;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeometryGrupp3.Controllers
+{
+    public class SquareController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Square square)
+        {
+            var inputRadius = square.Side;
+
+            var area = square.GetArea();
+            var perimeter = square.GetPerimeter();
+            ViewData["area"] = area;
+            ViewData["perimeter"] = perimeter;
+            return View();
+        }
+
+    }
+}
