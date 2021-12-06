6a8ea062287abfc3a049e04bfe3790c13fcedcd7
Mikael Larsson <73987306+84miklar@users.noreply.github.com>
2021-12-06 08:59:43 +0100

    small refactor

--- GeometryGrupp3/Controllers/SquareController.cs
+++ GeometryGrupp3/Controllers/SquareController.cs
@@ -1,9 +1,5 @@
 ﻿using GeometriTDD.Geometry;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GeometryGrupp3.Controllers
 {
@@ -13,18 +9,14 @@ namespace GeometryGrupp3.Controllers
         {
             return View();
         }
-
         [HttpPost]
         public IActionResult Index(Square square)
         {
-            var inputRadius = square.Side;
-
             var area = square.GetArea();
             var perimeter = square.GetPerimeter();
             ViewData["area"] = area;
             ViewData["perimeter"] = perimeter;
             return View();
         }
-
     }
 }
