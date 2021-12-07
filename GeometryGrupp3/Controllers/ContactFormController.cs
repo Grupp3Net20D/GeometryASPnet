73b0f6cd1dd3258bae52e2374771d9aee7308843
Mikael Larsson <73987306+84miklar@users.noreply.github.com>
2021-12-07 09:43:07 +0100

    Changed some css on results

--- GeometryGrupp3/Controllers/ContactFormController.cs
+++ GeometryGrupp3/Controllers/ContactFormController.cs
@@ -25,34 +25,6 @@ namespace GeometryGrupp3.Controllers
             return View();
         }
 
-
-        [HttpPost]
-        public IActionResult Index(ContactForm model)
-        {
-            string input = $"Följande person har skrivit till dig via kontaktformuläret:\n\nNamn:{ model.Fname} { model.Lname}\nEmail: { model.Email}\nMeddelande: { model.Msg}";
-            if (model.HasSubscribedToNewsLetter)
-            {
-                input += "\nLägg till personen som prenumerant av nyhetsbrevet.";
-            }
-
-                          //ViewData["input"] = input;
-
-                var desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-                var messageLog = Path.Combine(desktop, "Message.log");
-                System.IO.File.AppendAllText(messageLog, "\n\n" + DateTime.Now + ":\n" + input);
-
-                if (model.Fname == null || model.Lname == null || model.Email == null)
-                {
-
-                    return View();
-                }
-                else
-                {
-                    return View("../Home/Index");
-                }
-            
-        }
-    
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
